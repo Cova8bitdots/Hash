6e55100372bf83f5c38b7e58dfbd260285e37d3a
Cova8bitdots <cova.tech.8bitdots@gmail.com>
2019-04-29 17:20:41 +0900

    [Hash] DJBHashを追加

---
+++ DjbHash.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+/// <summary>
+/// http://www.cse.yorku.ca/~oz/hash.html
+/// </summary>
+public class DJBHash
+{
+    /// <summary> Hashの初期値 </summary>
+    const ulong HASH = 5381;
+
+    const int BIT_SHIFT = 5;// hash = hash * 33 +c の為の定数(らしい)
+
+    /// <summary>
+    /// DJB2 によるHash計算アルゴリズム
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    public static ulong Hash( string str )
+    {
+        ulong hash = HASH;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            hash = (( hash << BIT_SHIFT ) + hash) + str[i];
+        }
+
+        return hash;
+    }
+
+    /// <summary>
+    /// Hashの計算(バイナリデータから)
+    /// 正直String変換がめちゃくちゃ重いので非推奨
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    public static ulong Hash( byte[] data )
+    {
+        return Hash( Encoding.UTF8.GetString(data));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Hash
 Hash Algorithm for C#
+
+## DJB2
+- Bernstein 氏のHashアルゴリズム
+- マジックナンバーの論拠が不明だが動く
+- 文字列に対するHash値計算メソッド
+- 分布の偏りがあり、衝突もおきやすい。だがシンプル
+
+
